d13d3ab77926a391b2fc7c2e468d83d2fb39001e
Supankar Banik <supankar@cefalo.com>
2021-07-10 22:39:43 +0600

    webpack5 basic setup

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+import '../styles/index.scss';
+
+import { sum, sub } from './math';
+
+console.log('Webpack5 basic.....');
+
+console.log(sum(10, 20));
+console.log(sub(20, 10));
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: "./src/scripts/index.js",
+
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist")
+  },
+
+  mode: "development",
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9001,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+          options: {
+            minimize: false,
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+              publicPath: "images/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist']
+    })
+  ]
+};
